6c99a15717395ecc4c33799f94f425fa91cfc3a9
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-16 16:35:04 +0000

    feat(continente-api): add observability services and log aggregator

--- apps/external-apis/continente-api/docker/entrypoint.sh
+++ apps/external-apis/continente-api/docker/entrypoint.sh
@@ -5,4 +5,4 @@ echo "==> Running Laravel optimize:clear..."
 php artisan optimize:clear || true
 
 echo "==> Starting PHP server..."
-exec php -S 0.0.0.0:80 -t public
+exec php -S 0.0.0.0:80 -t public 2>&1
