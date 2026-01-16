771c276c2f899b7897ed8e4e31c3e7d9bbedbc26
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-16 15:20:06 +0000

    feat(continente-api): identify instance host

--- apps/external-apis/continente-api/bootstrap/app.php
+++ apps/external-apis/continente-api/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Infra\Http\Middleware\SetHostMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(SetHostMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
