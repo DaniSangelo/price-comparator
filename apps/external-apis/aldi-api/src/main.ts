159e61e3aa1449fde0d9b40fe50e513110c864fe
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-21 17:59:16 +0000

    fix(aldi-api): fix docker configs

--- apps/external-apis/aldi-api/src/main.ts
+++ apps/external-apis/aldi-api/src/main.ts
@@ -24,6 +24,6 @@ app.use(routes);
 
 const port = Number(process.env.PORT) || 3333;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     logger.info(`${process.env.APP_NAME} server started on port ${port}`);
 });
