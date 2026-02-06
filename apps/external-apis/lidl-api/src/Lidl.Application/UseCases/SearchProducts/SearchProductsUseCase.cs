e4110cf55be3ab4499858d04507759376212f2ae
Daniel Sângelo <danielsangelo@hotmail.com>
2026-02-06 17:53:10 +0000

    feat(lidl-api): implement use case to retrieve products according query param

---
+++ apps/external-apis/lidl-api/src/Lidl.Application/UseCases/SearchProducts/SearchProductsUseCase.cs
@@ -0,0 +1,42 @@
+using Lidl.Application.Ports;
+using Lidl.Domain.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Lidl.Application.UseCases.SearchProducts;
+
+public sealed class SearchProductsUseCase
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ILogger<SearchProductsUseCase> _logger;
+
+    public SearchProductsUseCase(IProductRepository productRepository, ILogger<SearchProductsUseCase> logger)
+    {
+        _productRepository = productRepository;
+        _logger = logger;
+    }
+
+    public async Task<SearchProductsOutput> ExecuteAsync(SearchProductsInput input, CancellationToken cancellationToken = default)
+    {
+        _logger.LogInformation("Searching products", input);
+        var page = Math.Max(1, input.Page);
+        var limit = Math.Clamp(input.Limit, 1, 200);
+        input.Page = page;
+        input.Limit = limit;
+
+        var (items, total) = await _productRepository.SearchAsync(input, cancellationToken);
+        var itemsDto = items.Select(MapToDto).ToList();
+        _logger.LogInformation("Products found: {Total}", total);
+        return new SearchProductsOutput(
+            Items: itemsDto,
+            Meta: new
+            { 
+                total,
+                page,
+                limit,
+                last_page = Math.Ceiling((double)total / limit) 
+            }
+        );
+    }
+
+    private static SearchProductsItemDto MapToDto(Product product) => new(product.Id, product.Name, product.Description, product.Category, product.Price, product.Currency, product.InStock, product.Brand, product.Image, product.UpdatedAt);
+}
