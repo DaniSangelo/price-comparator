6b18f4637410028d54ccbd5b7576ffe2c3ef58be
Daniel Sângelo <danielsangelo@hotmail.com>
2026-02-06 18:05:05 +0000

    feat(lidl-api): add constructor to input class

--- apps/external-apis/lidl-api/src/Lidl.Application/UseCases/SearchProducts/SearchProductsInput.cs
+++ apps/external-apis/lidl-api/src/Lidl.Application/UseCases/SearchProducts/SearchProductsInput.cs
@@ -5,4 +5,11 @@ public sealed class SearchProductsInput
     public string? Query { get; set; }
     public int Page { get; set; } = 1;
     public int Limit { get; set; } = 20;
+
+    public SearchProductsInput(string? query, int page, int limit)
+    {
+        Query = query;
+        Page = page;
+        Limit = limit;
+    }
 }
